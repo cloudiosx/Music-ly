d31a01ce4248621e9768f7d8bffbd04bafc9a02c
John Kim <cloudiosx@gmail.com>
2021-12-16 07:36:54 -0500

    Complete users model

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,25 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import datetime
+from sqlalchemy import DateTime
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+    __tablename__ = "users"
+
+    # Columns
 
     id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(40), nullable=False, unique=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    photoURL = db.Column(db.String(2000), nullable=False)
+    s3Name = db.Column(db.String(2000))
+    verified = db.Column(db.Boolean, default=False, nullable=False)
+    created_at = db.Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(DateTime, default=datetime.datetime.utcnow)
 
     @property
     def password(self):
@@ -23,8 +33,4 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password, password)
 
     def to_dict(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
-        }
+        return {"id": self.id, "username": self.username, "email": self.email}
