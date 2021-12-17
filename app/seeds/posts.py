ab1dea6f97bb77dd0e4f7b74b4c1ba0c162d17f3
John Kim <cloudiosx@gmail.com>
2021-12-17 12:00:37 -0500

    Add post seeder data structure

---
+++ app/seeds/posts.py
@@ -0,0 +1,54 @@
+from app.models import db, Video
+
+
+def seed_posts():
+    johnPost1 = Video(
+        userId=1,
+        videoURL="",
+        s3Name="s3Name",
+        videoType="comment",
+        topic="comedy",
+        music="avicci",
+        caption="This is sick",
+    )
+    jackiePost1 = Video(
+        userId=2,
+        videoURL="",
+        s3Name="s3Name",
+        videoType="comment",
+        topic="comedy",
+        music="avicci",
+        caption="This is sick",
+    )
+    davidPost1 = Video(
+        userId=3,
+        videoURL="",
+        s3Name="s3Name",
+        videoType="comment",
+        topic="comedy",
+        music="avicci",
+        caption="This is sick",
+    )
+
+    db.session.add(johnPost1)
+    db.session.add(jackiePost1)
+    db.session.add(davidPost1)
+
+    db.session.commit()
+
+
+def undo_posts():
+    db.session.execute("TRUNCATE videos RESTART IDENTITY CASCADE;")
+    db.session.commit()
+
+
+# id = db.Column(db.Integer, primary_key=True)
+# userId = db.Column(db.Integer, db.ForeignKey("users.id"))
+#     videoURL = db.Column(db.String(2000), nullable=False)
+#     s3Name = db.Column(db.String(2000))
+#     videoType = db.Column(db.String(40), nullable=False)
+#     topic = db.Column(db.String(255), nullable=False)
+#     music = db.Column(db.String(255))
+#     caption = db.Column(db.String(2000))
+#     created_at = db.Column(DateTime, default=datetime.datetime.utcnow)
+#     updated_at = db.Column(DateTime, default=datetime.datetime.utcnow)
