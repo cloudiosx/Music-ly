ff9e8e8a89408c13abac8d729f7e4c361e957014
John Kim <cloudiosx@gmail.com>
2021-12-19 16:54:16 -0500

    Setup layout for all routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,6 +10,7 @@ import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session/actions';
 import { mainLayoutRoutes } from './routes'
+import MainLayoutTemplate from './layout/MainLayoutTemplate'
 
 function App() {
   const [loaded, setLoaded] = useState(false);
---
+++ react-app/src/components/layout/MainLayoutTemplate.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import { Route } from 'react-router-dom';
+import './MainLayoutTemplate.css';
+import LeftSidebar from './LeftSidebar';
+import ContentWrapper from './ContentWrapper';
+
+function MainLayout(props) {
+  return (
+    <div className='page_main-layout'>
+      <NavBar />
+      <div className='main-layout-wrapper'>
+        <LeftSidebar />
+        <ContentWrapper>{props.children}</ContentWrapper>
+      </div>
+    </div>
+  );
+}
+
+function MainLayoutTemplate({ Component, ...props }) {
+  return (
+    <Route
+      {...props}
+      render={(propsOfComponent) => (
+        <MainLayout>
+          <Component {...propsOfComponent} />
+        </MainLayout>
+      )}
+    />
+  )
+}
+
+export default MainLayoutTemplate;
