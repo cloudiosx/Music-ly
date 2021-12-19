bdfa4fa5e983376131956cdaae46905754691344
John Kim <cloudiosx@gmail.com>
2021-12-19 15:45:40 -0500

    Add one post and delete post constants in post store

--- react-app/src/store/post/constants.js
+++ react-app/src/store/post/constants.js
@@ -3,3 +3,10 @@
 export const ALL_POSTS_SUCCESS = 'post/ALL_POSTS/SUCCESS';
 export const ALL_POSTS_LOADING = 'post/ALL_POSTS/LOADING';
 export const ALL_POSTS_ERROR = 'post/ALL_POSTS/ERROR';
+
+export const ONE_POST_SUCCESS = 'post/ONE_POST/SUCCESS';
+export const ONE_POST_LOADING = 'post/ONE_POST/LOADING';
+export const ONE_POST_ERROR = 'post/ONE_POST/ERROR';
+
+export const DELETE_POST_ERROR = 'post/DELETE_POST/ERROR';
+export const DELETE_POST_SUCCESS = 'post/DELETE_POST/SUCCESS';
