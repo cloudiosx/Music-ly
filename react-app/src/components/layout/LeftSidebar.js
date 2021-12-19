8493e9e4e3f0b9acf70af6e6099f29efe2ad305a
John Kim <cloudiosx@gmail.com>
2021-12-19 17:02:36 -0500

    Add sidebar to MainLayoutTemplate

---
+++ react-app/src/components/layout/LeftSidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Discover from '../components/Sidebar/Discorver';
+import ForYou from '../components/Sidebar/ForYou';
+import LoginSide from '../components/Sidebar/LoginSide';
+import SuggestedAccounts from '../components/Sidebar/SuggestedAccounts';
+import './MainLayoutTemplate.css';
+
+const LeftSidebar = ({ children }) => {
+  return (
+    <div className="main-layout-sidebar">
+      <div className="sidebar-lock">
+        <ForYou />
+        <LoginSide />
+        <SuggestedAccounts />
+        <Discover />
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
