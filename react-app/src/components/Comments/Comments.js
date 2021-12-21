7b2f60c374998cef050e21f19b5520844e514679
John Kim <cloudiosx@gmail.com>
2021-12-21 01:32:15 -0500

    Complete comments.js structure

---
+++ react-app/src/components/Comments/Comments.js
@@ -0,0 +1,240 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import './Comments.css';
+
+const Comments = (props) => {
+  return (
+    <div className="comments">
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+      {/* One comment */}
+      <div className="comments_item">
+        <div className="comments_item_content">
+          <span className="avatar_comment">
+            <img src="/images/user1.jpg" alt="user 1" />
+          </span>
+          <div className="content_container">
+            {/* TODO: link to user profile */}
+            <NavLink to="/users" className="user_link">
+              This username's comment
+            </NavLink>
+            <p className="comment_text">
+              <span>My comment here</span>
+              <div className="comment_text_time">
+                <span>6d ago</span>
+              </div>
+            </p>
+          </div>
+          <div className="like_container">
+            <i className="far fa-heart"></i>
+            <span>600K</span>
+          </div>
+        </div>
+      </div>
+      {/* End one comment */}
+    </div>
+  );
+};
+
+Comments.propTypes = {};
+
+export default Comments;
