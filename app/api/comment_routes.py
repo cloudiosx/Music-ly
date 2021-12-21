c7da394a82ae6902fe0042f94363032f41f5ceb3
John Kim <cloudiosx@gmail.com>
2021-12-21 16:11:25 -0500

    Add backend routes for comment

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,22 +1,35 @@
 from flask import Blueprint, request, jsonify
-from flask_login import login_required
-from app.models import Comment
+from app.models import db, Comment
 
 comment_routes = Blueprint("comments", __name__)
 
 # POST /comments/new
 @comment_routes.route("/new", methods=["POST"])
 def new_comment():
-    return "We are in the create comment route"
+    comment = Comment(
+        userId=request.json["userId"],
+        postId=request.json["videoId"],
+        comment=request.json["content"],
+    )
+    db.session.add(comment)
+    db.session.commit()
+    return jsonify(comment.to_dict())
 
 
 # UPDATE /comments/:id/edit
 @comment_routes.route("/<int:id>/edit", methods=["POST"])
-def update_comment():
-    return "We are in the edit comment route"
+def update_comment(id):
+    commentToUpdate = Comment.query.get(id)
+    commentToUpdate.content = request.json["content"]
+    db.session.commit()
+    updatedComment = Comment.query.get(id)
+    return jsonify(updatedComment.to_dict())
 
 
 # DELETE /comments/:id
 @comment_routes.route("/<int:id>", methods=["DELETE"])
-def delete_comment():
-    return "We are in the delete comment route"
+def delete_comment(id):
+    commentToDelete = Comment.query.get(id)
+    db.session.delete(commentToDelete)
+    db.session.commit()
+    return jsonify(commentToDelete.to_dict())
