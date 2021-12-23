23d486d6916778d3acce137f472f46e245fd596e
John Kim <cloudiosx@gmail.com>
2021-12-23 07:31:14 -0500

    Update Feed.js

--- react-app/src/components/Feed/Feed.js
+++ react-app/src/components/Feed/Feed.js
@@ -1,15 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import { getAllPosts } from '../../store/post/actions';
 import Post from '../Post/Post';
 import './Feed.css';
 
 function Feed() {
-  const history = useHistory();
   const dispatch = useDispatch();
-
-  // useSelectors
 
   const allPosts = useSelector((state) => state.postStore.allPosts);
   console.log('allPosts ===> ', allPosts);
