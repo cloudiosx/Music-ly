194414a321f9ab4b7f56d6a79e599904910db2eb
John Kim <cloudiosx@gmail.com>
2021-12-24 17:47:16 -0500

    Fix post route current user logic

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -8,36 +8,60 @@ post_routes = Blueprint("posts", __name__)
 # GET /posts
 @post_routes.route("/")
 def posts():
-    posts = (
-        Video.query.join(User, User.id == Video.userId)
-        .add_columns(User.fullname, User.username, User.photoURL)
-        .order_by(desc(Video.created_at))
-        .all()
-    )
-    user = User.query.get(current_user.to_dict()["id"])
-    newList = []
-    for post in posts:
-        postDetails = post[0].to_dict()
-        postDetails["User"] = {
-            "fullname": post[1],
-            "username": post[2],
-            "photoURL": post[3],
-        }
-        # if postDetails["userId"] in post[0].likesOfVideo.all():
-        #     isLiked = True
-        # else:
-        #     isLiked = False
-        if user in post[0].likesOfVideo.all():
-            isLiked = True
-        else:
-            isLiked = False
-        postDetails["isLiked"] = isLiked
-        totalLikes = len(post[0].likesOfVideo.all())
-        totalComments = len(Comment.query.filter_by(videoId=post[0].id).all())
-        postDetails["totalLikes"] = totalLikes
-        postDetails["totalComments"] = totalComments
-        newList.append(postDetails)
-    return jsonify(newList)
+    if current_user.is_authenticated:
+        posts = (
+            Video.query.join(User, User.id == Video.userId)
+            .add_columns(User.fullname, User.username, User.photoURL)
+            .order_by(desc(Video.created_at))
+            .all()
+        )
+        newList = []
+        for post in posts:
+            postDetails = post[0].to_dict()
+            postDetails["User"] = {
+                "fullname": post[1],
+                "username": post[2],
+                "photoURL": post[3],
+            }
+            user = User.query.get(current_user.to_dict()["id"])
+            if user in post[0].likesOfVideo.all():
+                isLiked = True
+            else:
+                isLiked = False
+            postDetails["isLiked"] = isLiked
+            totalLikes = len(post[0].likesOfVideo.all())
+            totalComments = len(Comment.query.filter_by(videoId=post[0].id).all())
+            postDetails["totalLikes"] = totalLikes
+            postDetails["totalComments"] = totalComments
+            newList.append(postDetails)
+        return jsonify(newList)
+    else:
+        posts = (
+            Video.query.join(User, User.id == Video.userId)
+            .add_columns(User.fullname, User.username, User.photoURL)
+            .order_by(desc(Video.created_at))
+            .all()
+        )
+        newList = []
+        for post in posts:
+            postDetails = post[0].to_dict()
+            postDetails["User"] = {
+                "fullname": post[1],
+                "username": post[2],
+                "photoURL": post[3],
+            }
+            user = User.query.get(postDetails["userId"])
+            if user in post[0].likesOfVideo.all():
+                isLiked = True
+            else:
+                isLiked = False
+            postDetails["isLiked"] = isLiked
+            totalLikes = len(post[0].likesOfVideo.all())
+            totalComments = len(Comment.query.filter_by(videoId=post[0].id).all())
+            postDetails["totalLikes"] = totalLikes
+            postDetails["totalComments"] = totalComments
+            newList.append(postDetails)
+        return jsonify(newList)
 
 
 # GET /posts/filtered
@@ -49,25 +73,44 @@ def filtered_posts():
 # GET /posts/:id
 @post_routes.route("/<int:id>")
 def post(id):
-    post = Video.query.get(id)
-    postDetails = post.to_dict()
-    # user = User.query.get(postDetails["userId"])
-    user = User.query.get(current_user.to_dict()["id"])
-    userDetails = user.to_dict()
-    if user in post.likesOfVideo.all():
-        isLiked = True
+    if current_user.is_authenticated:
+        post = Video.query.get(id)
+        postDetails = post.to_dict()
+        user = User.query.get(current_user.to_dict()["id"])
+        userDetails = user.to_dict()
+        if user in post.likesOfVideo.all():
+            isLiked = True
+        else:
+            isLiked = False
+        totalLikes = len(post.likesOfVideo.all())
+        totalComments = len(Comment.query.filter_by(videoId=id).all())
+        returnObject = {
+            "User": {**userDetails},
+            **postDetails,
+            "isLiked": isLiked,
+            "totalLikes": totalLikes,
+            "totalComments": totalComments,
+        }
+        return returnObject
     else:
-        isLiked = False
-    totalLikes = len(post.likesOfVideo.all())
-    totalComments = len(Comment.query.filter_by(videoId=id).all())
-    returnObject = {
-        "User": {**userDetails},
-        **postDetails,
-        "isLiked": isLiked,
-        "totalLikes": totalLikes,
-        "totalComments": totalComments,
-    }
-    return returnObject
+        post = Video.query.get(id)
+        postDetails = post.to_dict()
+        user = User.query.get(postDetails["userId"])
+        userDetails = user.to_dict()
+        if user in post.likesOfVideo.all():
+            isLiked = True
+        else:
+            isLiked = False
+        totalLikes = len(post.likesOfVideo.all())
+        totalComments = len(Comment.query.filter_by(videoId=id).all())
+        returnObject = {
+            "User": {**userDetails},
+            **postDetails,
+            "isLiked": isLiked,
+            "totalLikes": totalLikes,
+            "totalComments": totalComments,
+        }
+        return returnObject
 
 
 # POST /posts/new
