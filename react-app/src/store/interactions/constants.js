aa2b4f57e6bdf851168893fb23a4c0aa610c21c7
John Kim <cloudiosx@gmail.com>
2021-12-24 12:21:30 -0500

    Modify interactions store constants

--- react-app/src/store/interactions/constants.js
+++ react-app/src/store/interactions/constants.js
@@ -1,2 +1,5 @@
 export const COMMENT_SUCCESS = 'comment/COMMENTS/SUCCESS';
 export const COMMENT_FAIL = 'comment/COMMENTS/FAIL';
+
+export const DELETE_COMMENT_SUCCESS = 'comment/DEL_COMMENTS/SUCCESS';
+export const DELETE_COMMENT_FAIL = 'comment/DEL_COMMENTS/FAIL';
