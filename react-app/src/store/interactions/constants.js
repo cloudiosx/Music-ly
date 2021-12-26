904aead4e238149c6b19148ab387a6ca908e8f32
John Kim <cloudiosx@gmail.com>
2021-12-26 12:05:02 -0500

    Add constants to Interactions store

--- react-app/src/store/interactions/constants.js
+++ react-app/src/store/interactions/constants.js
@@ -3,3 +3,6 @@ export const COMMENT_FAIL = 'comment/COMMENTS/FAIL';
 
 export const DELETE_COMMENT_SUCCESS = 'comment/DEL_COMMENTS/SUCCESS';
 export const DELETE_COMMENT_FAIL = 'comment/DEL_COMMENTS/FAIL';
+
+export const LIKE_POST_SUCCESS = 'comment/LIKE/SUCCESS';
+export const LIKE_POST_FAIL = 'comment/LIKE/FAIL';
