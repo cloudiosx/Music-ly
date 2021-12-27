01f2ff3b8e8c9e9eb6c245d8f9d092c3af22b05f
John Kim <cloudiosx@gmail.com>
2021-12-27 11:12:52 -0500

    Add current user check to follow new route

--- app/api/follow_routes.py
+++ app/api/follow_routes.py
@@ -8,16 +8,17 @@ follow_routes = Blueprint("followers", __name__)
 # POST /followers/new
 @follow_routes.route("/new", methods=["POST"])
 def new_follow():
-    post = Video.query.get(id)
-    postDetails = post.to_dict()
-    user = User.query.get(postDetails["userId"])
-    currentUser = User.query.get(current_user.to_dict()["id"])
+    if current_user.is_authenticated:
+        post = Video.query.get(id)
+        postDetails = post.to_dict()
+        user = User.query.get(postDetails["userId"])
+        currentUser = User.query.get(current_user.to_dict()["id"])
 
-    if currentUser not in user.followings:
-        user.followings.append(currentUser)
-        db.session.commit()
-        return "You have followed the user successfully"
-    else:
-        user.followings.remove(currentUser)
-        db.session.commit()
-        return "You have unfollowed the user successfully"
+        if currentUser not in user.followings:
+            user.followings.append(currentUser)
+            db.session.commit()
+            return "You have followed the user successfully"
+        else:
+            user.followings.remove(currentUser)
+            db.session.commit()
+            return "You have unfollowed the user successfully"
