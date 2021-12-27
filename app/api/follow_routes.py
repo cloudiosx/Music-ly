7c0a534d2fe08071cc4aab6fb129c18e0f58663c
John Kim <cloudiosx@gmail.com>
2021-12-27 11:09:36 -0500

    Modify follow routes to include making a follow

--- app/api/follow_routes.py
+++ app/api/follow_routes.py
@@ -1,10 +1,23 @@
 from flask import Blueprint, jsonify, request
 from sqlalchemy.orm.session import Session
-from app.models import followers, User, db
+from flask_login import current_user
+from app.models import followers, User, Video, db
 
 follow_routes = Blueprint("followers", __name__)
 
 # POST /followers/new
 @follow_routes.route("/new", methods=["POST"])
 def new_follow():
-    return "We are in the new follow route"
+    post = Video.query.get(id)
+    postDetails = post.to_dict()
+    user = User.query.get(postDetails["userId"])
+    currentUser = User.query.get(current_user.to_dict()["id"])
+
+    if currentUser not in user.followings:
+        user.followings.append(currentUser)
+        db.session.commit()
+        return "You have followed the user successfully"
+    else:
+        user.followings.remove(currentUser)
+        db.session.commit()
+        return "You have unfollowed the user successfully"
