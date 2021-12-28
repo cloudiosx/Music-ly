38ab5c27906b01913330efb75cf78a78885334e8
John Kim <cloudiosx@gmail.com>
2021-12-28 11:36:42 -0500

    Modify NavBar.js structure

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,27 +1,18 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { useLoginContext } from '../../Context/LoginProvider';
 import { logout } from '../../store/session/actions';
-import LoginModal from '../auth/AuthForm/LoginModal';
-import SignUpModal from '../auth/AuthForm/SignUpModal';
 import Button from '../pieces/Button';
 import Modal from '../pieces/Modal';
 import SearchBar from '../SearchBar/SearchBar';
 import UploadPost from '../UploadPost/UploadPost';
 import './NavBar.css';
 
-const ModalView = {
-  login: 'login',
-  signup: 'signup',
-};
-
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [modalView, setModalView] = useState('');
 
-  const closeModal = () => setModalView('');
-  const openLoginModal = () => setModalView(ModalView.login);
-  const openSignUpModal = () => setModalView(ModalView.signup);
+  const { openLoginModal } = useLoginContext();
 
   const { user } = useSelector((state) => state.session);
   const dispatch = useDispatch();
@@ -40,7 +31,7 @@ const NavBar = () => {
         <li>
           <NavLink className="nav-bar-logo-link" to="/" exact={true} activeClassName="active">
             <div className="nav-bar-logo">
-              <img src="https://tiktok-react-cloudiosx.s3.us-east-2.amazonaws.com/SvgImages/logo.svg" alt="music.ly logo"/>
+              <img src="/images/logo.svg" alt="music.ly logo" />
             </div>
           </NavLink>
         </li>
@@ -65,7 +56,7 @@ const NavBar = () => {
                 </div>
               ) : (
                 <div className="button-upload" onClick={toggleModalUpload}>
-                  <img src="https://tiktok-react-cloudiosx.s3.us-east-2.amazonaws.com/SvgImages/uploadCloud.svg" alt="upload video" />
+                  <img src="/images/uploadCloud.svg" alt="upload video" />
                 </div>
               )}
             </li>
@@ -88,8 +79,6 @@ const NavBar = () => {
       <Modal isOpen={isOpen} onClose={toggleModalUpload}>
         <UploadPost />
       </Modal>
-      <LoginModal isOpen={modalView === ModalView.login} onClose={closeModal} onClickFooterAction={openSignUpModal} />
-      <SignUpModal isOpen={modalView === ModalView.signup} onClose={closeModal} onClickFooterAction={openLoginModal} />
     </nav>
   );
 };
