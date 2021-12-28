ce5f3e06b2a1f7e7e84b7b1ca2a9d5069b1e50d1
John Kim <cloudiosx@gmail.com>
2021-12-28 14:05:20 -0500

    Modify follow route

--- app/api/follow_routes.py
+++ app/api/follow_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from sqlalchemy.orm.session import Session
 from flask_login import current_user
 from app.models import followers, User, Video, db
+import ast
 
 follow_routes = Blueprint("followers", __name__)
 
@@ -9,7 +10,10 @@ follow_routes = Blueprint("followers", __name__)
 @follow_routes.route("/new", methods=["POST"])
 def new_follow():
     if current_user.is_authenticated:
-        post = Video.query.get(id)
+        requestBody = ast.literal_eval(request.data.decode("utf-8"))
+        postId = requestBody["postId"]
+
+        post = Video.query.get(postId)
         postDetails = post.to_dict()
         user = User.query.get(postDetails["userId"])
         currentUser = User.query.get(current_user.to_dict()["id"])
