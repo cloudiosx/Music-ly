6b5a056006492424a2abf5018e25778da3538761
John Kim <cloudiosx@gmail.com>
2021-12-30 10:51:15 -0500

    Complete ModalAuth.js

--- react-app/src/components/auth/ModalAuth/ModalAuth.js
+++ react-app/src/components/auth/ModalAuth/ModalAuth.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Button from '../../pieces/Button';
 import Modal from '../../pieces/Modal';
 import './ModalAuth.css';
 
 const ModalAuth = (props) => {
-  const { isOpen, onClose, children, title, onFooterAction, footerQuestion, footerAction } = props;
+  const { isOpen, onClose, children, title, onFooterAction, footerQuestion, footerAction, demoLogin } = props;
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} style={{ minWidth: 'unset', width: '520px', height: '780px' }}>
@@ -15,10 +16,17 @@ const ModalAuth = (props) => {
 
         <div className="auth_modal_footer">
           <div className="auth_modal_footer_content">
-            <div>{footerQuestion}</div>{' '}
-            <span className="auth_modal_footer--button active_link" onClick={onFooterAction}>
-              {footerAction}
-            </span>
+            <div style={{ display: 'flex' }}>
+              <div>{footerQuestion}</div>{' '}
+              <span className="auth_modal_footer--button active_link" onClick={onFooterAction}>
+                {footerAction}
+              </span>
+            </div>
+            {demoLogin && (
+              <div className="auth_modal_footer--demo_login">
+                <Button onClick={demoLogin}>Demo Login</Button>
+              </div>
+            )}
           </div>
         </div>
       </div>
