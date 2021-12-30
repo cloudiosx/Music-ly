04b77601180d5e93151df78ab462e04b29ee838a
John Kim <cloudiosx@gmail.com>
2021-12-30 12:13:19 -0500

    Add About links

--- react-app/src/layout/LeftSidebar.js
+++ react-app/src/layout/LeftSidebar.js
@@ -9,6 +9,7 @@ import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
 import './MainLayoutTemplate.css';
+import About from '../components/Sidebar/About';
 
 const LeftSidebar = ({ children }) => {
   const user = useSelector((state) => state.session.user);
@@ -23,6 +24,7 @@ const LeftSidebar = ({ children }) => {
         {location.pathname !== '/following' && <SuggestedAccounts />}
         {user && <FollowingList />}
         <Discover />
+        <About />
       </div>
     </div>
   );
