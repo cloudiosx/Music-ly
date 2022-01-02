6b5206fdeb44555098fc18e7935f5d91133382a7
John Kim <cloudiosx@gmail.com>
2022-01-01 19:37:37 -0500

    Update follow routes

--- app/api/follow_routes.py
+++ app/api/follow_routes.py
@@ -11,11 +11,12 @@ follow_routes = Blueprint("followers", __name__)
 def new_follow():
     if current_user.is_authenticated:
         requestBody = ast.literal_eval(request.data.decode("utf-8"))
-        postId = requestBody["postId"]
+        # postId = requestBody["postId"]
+        userId = requestBody["userId"]
 
-        post = Video.query.get(postId)
-        postDetails = post.to_dict()
-        user = User.query.get(postDetails["userId"])
+        # post = Video.query.get(postId)
+        # postDetails = post.to_dict()
+        user = User.query.get(userId)
         currentUser = User.query.get(current_user.to_dict()["id"])
 
         if currentUser not in user.followings:
