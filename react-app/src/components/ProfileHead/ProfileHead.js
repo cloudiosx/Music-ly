b991a9d0d01a1208229296c0d8ab794a4540da74
John Kim <cloudiosx@gmail.com>
2022-01-01 19:41:40 -0500

    Modify ProfileHead.js

--- react-app/src/components/ProfileHead/ProfileHead.js
+++ react-app/src/components/ProfileHead/ProfileHead.js
@@ -1,47 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '../pieces/Button';
 import './ProfileHead.css';
+import { updateFollow } from '../../store/interactions/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLoginContext } from '../../Context/LoginProvider';
+import { useParams } from 'react-router-dom';
 
-const ProfileHead = () => {
+const ProfileHead = ({ userProfile }) => {
+  const { userId } = useParams();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const { openLoginModal } = useLoginContext();
+
+  console.log("user ======>", user)
+
+  const isMe = userId === user?.id?.toString();
+
+  const toggleFollow = () => {
+    if (!user) {
+      openLoginModal();
+      return;
+    }
+    dispatch(updateFollow({ userId: userId }));
+  };
+
   return (
     <header>
       <div className="profile_userinfo">
         <div className="profile_userinfo_avatar">
-          <img src="/images/user1.jpg" alt="user 1" />
+          <img src={userProfile.photoURL} alt="user 1" />
         </div>
         <div className="profile_userinfo_right">
-          <h1 className="profile_userinfo_right--title">Username</h1>
-          <h2 className="profile_userinfo_right--subtitle">Channel name</h2>
-          <Button type="fill" className="profile_userinfo_right--button">
-            Follow
-          </Button>
+          <h1 className="profile_userinfo_right--title">{userProfile.username}</h1>
+          <h2 className="profile_userinfo_right--subtitle">{userProfile.fullname}</h2>
+          {/* TODO: handel follow */}
+          {!isMe && (
+            <Button
+              onClick={toggleFollow}
+              type={`${userProfile.isFollowed ? 'text' : 'fill'}`}
+              className={`profile_userinfo_right--button ${userProfile.isFollowed ? 'following' : ''}`}
+            >
+              {userProfile.isFollowed ? 'Following' : 'Follow'}
+            </Button>
+          )}
         </div>
       </div>
 
       <div className="profile_stats">
         <div className="stats_item">
-          <strong>999</strong>
+          <strong>{userProfile.totalFollowings}</strong>
           <span className="stats_item--label">Following</span>
         </div>
         <div className="stats_item">
-          <strong>999</strong>
+          <strong>{userProfile.totalFollowers}</strong>
           <span className="stats_item--label">Followers</span>
         </div>
         <div className="stats_item">
-          <strong>999</strong>
+          <strong>{userProfile.totalLikes}</strong>
           <span className="stats_item--label">Likes</span>
         </div>
       </div>
 
-      <div className="profile_bio">My bio description</div>
+      {/* <div className="profile_bio">My bio description</div>
       <div className="profile_links">
         <a href="https://google.com">
           <i class="fas fa-link fa-sm"></i>
           google.com
         </a>
-      </div>
+      </div> */}
     </header>
   );
 };
 
+ProfileHead.propTypes = {
+  userProfile: PropTypes.object,
+};
 export default ProfileHead;
