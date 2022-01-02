c065a074b4b3ba29bae72709a483d52f23f89463
John Kim <cloudiosx@gmail.com>
2022-01-02 17:30:18 -0500

    Fix comments CRUD

--- react-app/src/util/validation.js
+++ react-app/src/util/validation.js
@@ -1,6 +1,7 @@
 const isNotEmpty = (input) => !!input && !!input.length;
 const isLessThan = (input, max) => !!input && input.length < max;
 const isGreaterThan = (input, min) => !!input && input.length > min;
+// email address validation (https://stackoverflow.com/a/46181/104380
 const isEmail = (input) =>
   !!input &&
   input.match(
@@ -26,11 +27,11 @@ const getErrorCheck = (input, typeCheck, options) => {
     case 'max':
       return isLessThan(input, options.max) ? '' : `*Field must less than ${options.max} characters`;
     case 'min':
-      return isGreaterThan(input, options.min) ? '' : `*Field must be greater than ${options.min} characters`;
+      return isGreaterThan(input, options.min) ? '' : `*Field must greater than ${options.min} characters`;
     case 'between':
       return isBetween(input, options)
         ? ''
-        : `*Field must be greater than ${options.min} and less than ${options.max} characters`;
+        : `*Field must greater than ${options.min} and less than ${options.max} characters`;
     default:
       return 'Field required';
   }
