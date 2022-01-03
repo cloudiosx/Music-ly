25cf5ac4d594b9da1f05dfee58ee6f2cccd40ee8
John Kim <cloudiosx@gmail.com>
2022-01-03 08:56:40 -0500

    Modify Following.js

--- react-app/src/components/Following/Following.js
+++ react-app/src/components/Following/Following.js
@@ -1,18 +1,38 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getAllPosts } from '../../store/post/actions';
 import { getAllUsersThatIDontFollow } from '../../store/user/actions';
 import CardFollow from '../CardFollow/CardFollow';
+import Post from '../Post/Post';
 import './Following.css';
 
 function Following() {
   const dispatch = useDispatch();
 
   const allUsers = useSelector((state) => state.userStore.allUsersThatIAmNotFollowing);
+  const allPosts = useSelector((state) => state.postStore.allPosts);
+  const user = useSelector((state) => state.session.user);
 
   useEffect(() => {
-    dispatch(getAllUsersThatIDontFollow());
+    if (user) {
+      if (user.totalFollowings) {
+        dispatch(getAllPosts({ following: true }));
+      } else {
+        dispatch(getAllUsersThatIDontFollow());
+      }
+    } else {
+      dispatch(getAllUsersThatIDontFollow());
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [user]);
+
+  if (user && !!user.totalFollowings && !!allPosts.length) {
+    return (
+      <main className="main_feed-container">
+        {allPosts && allPosts.map((post) => <Post post={post} key={post.id} />)}
+      </main>
+    );
+  }
 
   return (
     <main className="card_follow_wrap">
